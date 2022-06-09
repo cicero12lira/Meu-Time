6a91b5cd0d900779bb72aab29022dc167afbd213
cicero12lira <ciceroprr12@gmail.com>
2022-06-09 10:50:24 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,49 @@
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-Br', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+
+    function iniciaRelogio() {
+        timer = setInterval(function() {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
+    }
+
+    document.addEventListener('click', function(e) {
+        const el = e.target;
+
+        if (el.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            relogio.classList.remove('iniciado');
+            relogio.classList.add('zerado');
+            segundos = 0;
+        }
+
+        if (el.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            relogio.classList.remove('zerado');
+            relogio.classList.add('iniciado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+
+        if (el.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.remove('iniciado');
+            relogio.classList.remove('zerado');
+            relogio.classList.add('pausado');
+        }
+    });
+}
+relogio();
